1b047243796d2b590124a842ef21c43f1826dbac
Mnoisec <2469423174@qq.com>
2022-07-06 18:01:41 +0800

    Update test_refresh.py

--- page/test_refresh.py
+++ page/test_refresh.py
@@ -2,7 +2,7 @@ from base.basepage import BasePage
 from selenium.webdriver.common.by import By
 
 class Refresh(BasePage):
-    url = "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/60577518981592662729936491994831767198268555683308385638080151811416695767041"
+    url = "https://opensea.io/assets/matic/0xecc82095b2e23605cd95552d90216faa87606c40/3305"
     refresh_button = (By.XPATH,'//div[@class="item--collection-toolbar-wrapper"]/div[@class="sc-1xf18x6-0 sc-1twd32i-0 sc-1skvztv-1 haVRLx kKpYwv kgqWIT"]/button[1]')
     msg = (By.XPATH,'//div[@class="sc-1xf18x6-0 sc-1twd32i-0 eAciso kKpYwv"]/parent::div')
 
